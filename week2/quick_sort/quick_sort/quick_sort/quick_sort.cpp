499bdebcb1f4c15ab1c6e56d2ed2471e43ec712e
fryme <oleg.fry@gmail.com>
2016-07-23 17:08:13 +0300

    Complete week2 assignment

--- week2/quick_sort/quick_sort/quick_sort/quick_sort.cpp
+++ week2/quick_sort/quick_sort/quick_sort/quick_sort.cpp
@@ -4,7 +4,6 @@
 
 using namespace std;
 
-
 enum PivotStrategy
 {
 	First,
@@ -12,76 +11,96 @@ enum PivotStrategy
 	Last
 };
 
-int partition(std::vector<int>& array, int l, int r, int pivot)
+int partition_first(std::vector<int>& array, int l, int r)
 {
-	//cout << "l " << l << " r " << r << endl;
-	
-	int i = l + 1;
-
-	for (int j = i; j <= r; ++j)
+	int i = l + 1, pivot = array[l];
+	for (int j = i; j <= r; j++)
 	{
 		if (array[j] < pivot)
 		{
-			//cout << "swap: " << array[i] << " and " << array[j] << endl;
 			std::swap(array[i], array[j]);
-			//print_array(array, array.size());
-			i += 1;
+			i++;
 		}
 	}
 
-	std::swap(array[l], array[i-1]);
-	//print_array(array, array.size());
-	//cout << "i " << --i << endl;
-	--i;
-	return i;
+	std::swap(array[l], array[i - 1]);
+	return i - 1;
 }
 
-int partition_right(std::vector<int>& array, int l, int r)
+inline int find_median_of_three(std::vector<int>& array)
 {
-	int pivot = array[r];
-	int i = 
-	for (int )
+	int first_pos = 0;
+	int length = array.size();
+	int middle_pos = length % 2 ? length / 2 : (length / 2 - 1);
+	int last_pos = length - 1;
+
+	int first = array[first_pos];
+	int middle = array[middle_pos];
+	int last = array[last_pos];
+
+	if (first <= middle) 
+	{
+		if (middle <= last)
+			return middle_pos;
+		else if (first <= last)
+			return last_pos;
+		return first_pos;
+	}
+
+	if (first <= last)
+		return first_pos;
+
+	else if (middle <= last)
+		return last_pos;
+
+	return middle_pos;
 }
 
-void quick_sort(std::vector<int>& array, int l, int r, 
-				int& numberOfComparsions, PivotStrategy pivotStrategy)
+uint32_t quick_sort(std::vector<int>& array, int l, int r, PivotStrategy pivotStrategy)
 {
 	if (l < r)
 	{
-		int p(0);
-		if (pivotStrategy == First)
-			p = partition(array, l, r, array[l]);
-		else if (pivotStrategy == Middle)
-			p = partition(array, l, r, array[l]);
-		else
-			p = partition(array, l, r, array[r]);
+		uint32_t numberOfComparsions(r - l);
+		int pivotPos(0);
+		if (pivotStrategy == Middle)
+		{
+			int pivot = find_median_of_three(array);
+			std::swap(array[0], array[pivot]);
+		}
+		else if (pivotStrategy == Last)
+		{
+			std::swap(array[l], array[r]);
+		}
 
-		//cout << "p: " << p << " l: " << l << " r: " << r << endl;
+		pivotPos = partition_first(array, l, r);
 		
-		numberOfComparsions += (p - 1 - l - 1);
-		quick_sort(array, l, p - 1, numberOfComparsions, pivotStrategy);
-		numberOfComparsions += (r - p + 1 - 1);
-		quick_sort(array, p + 1, r, numberOfComparsions, pivotStrategy);
+		numberOfComparsions += quick_sort(array, l, pivotPos - 1, pivotStrategy);
+		numberOfComparsions += quick_sort(array, pivotPos + 1, r, pivotStrategy);
+		
+		return numberOfComparsions;
+	}
+	else
+	{
+		return 0;
 	}
 }
 
 int main()
 {
 	std::vector<int32_t> array;
-	
-	array.push_back(3);
-	array.push_back(4);
-	array.push_back(2);
-	array.push_back(6);
-	array.push_back(9);
-	array.push_back(1);
-	
-	//print_array(array, array.size());
-	int numberOfComparsions(0);
-	read_from_file(L"..//..//..//artefacts//_32387ba40b36359a38625cbb397eee65_QuickSort.txt", array);
 
-	quick_sort(array, 0, array.size() - 1, numberOfComparsions);
-	//print_array(array, array.size());
+	uint32_t numberOfComparsions(0);
+	read_from_file(L"..//..//..//artefacts//_32387ba40b36359a38625cbb397eee65_QuickSort.txt", array);
+	numberOfComparsions = quick_sort(array, 0, array.size() - 1, First);
 	cout << "numberOfComparsions = " << numberOfComparsions;
+
+	read_from_file(L"..//..//..//artefacts//_32387ba40b36359a38625cbb397eee65_QuickSort.txt", array);
+	numberOfComparsions = quick_sort(array, 0, array.size() - 1, Last);
+	cout << "numberOfComparsions = " << numberOfComparsions;
+
+	read_from_file(L"..//..//..//artefacts//_32387ba40b36359a38625cbb397eee65_QuickSort.txt", array);
+	numberOfComparsions = quick_sort(array, 0, array.size() - 1, Middle);
+	cout << "numberOfComparsions = " << numberOfComparsions;
+
 	return 0;
 }
